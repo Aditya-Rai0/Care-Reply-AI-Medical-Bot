d793bb53ca62e5ac1f7e88af81867bf939461fa3
Aditya Rai <adityarai29052005@gmail.com>
2025-07-19 18:59:56 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version = '0.0.0',
+    author = 'Aditya Rai',
+    author_email = 'adityarai29052005@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+)
